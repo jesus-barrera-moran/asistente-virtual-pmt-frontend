5ab582fd90d5f8deb5baa1897751c005bc95e886
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-19 23:19:27 -0600

    Eliminar archivos innecesarios

--- app/admin/databases/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DatabasesManager from '@/components/DatabasesManager';
-
-export default function DatabasesManagerPage() {
-  return <DatabasesManager />;
-}
--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import React from 'react';
-import PastryForm from '@/components/PastryForm';
-
-export default function RegisterPage() {
-  return (
-    <PastryForm />
-  );
-}
